9def3b441e18f258255c8f3db49f1f8a1a589a9f
asmamehraban6-beep <asmamehraban6@gmail.com>
2026-07-16 10:22:50 -0700

    fix duplicated portfolio in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,24 @@
 import Header from "../components/Header";
 import TypingAnimation from "../components/TypingAnimation";
-import Skills from "../components/Skills";
 import Projects from "./Projects";
 import Contact from "./Contact";
 
 
 function Home() {
 
-
   return (
 
-
     <>
-
 
       <Header />
 
       <TypingAnimation />
 
-
       <Projects />
 
-<Contact />
-  
-
-
+      <Contact />
 
     </>
-
 
   );
 
